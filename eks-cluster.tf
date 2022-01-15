c4fcebedb92a564f9bd7ad61828078cbe1f3eaba
KLN <karthik.kln@gmail.com>
2022-01-15 15:58:35 +0000

    minor change

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -13,13 +13,13 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
+      name                          = "application-worker-group-1"
       instance_type                 = "t2.small"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
-      name                          = "worker-group-2"
+      name                          = "plaform-worker-group-2"
       instance_type                 = "t2.medium"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
