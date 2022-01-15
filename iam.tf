d54486a2426d7646ef5b8e97f3152023d004da13
KLN <karthik.kln@gmail.com>
2022-01-15 16:05:19 +0000

    iam admin role added

---
+++ iam.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_role" "admin_role" {
+  name = "admin_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+locals {
+map_roles = [{
+rolearn= aws_iam_role.admin_role.arn
+username= "system:node:{{EC2PrivateDNSName}}"
+groups= ["cluster-admin" ]
+}
+]
+}
