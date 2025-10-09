e68dc66d697d036173d23d1b0a4a4d7492e6be1e
Lekshmi Kolappan <lekshmi.kolappan@extremereach.com>
2025-10-09 22:10:29 +0100

    cluster name change

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "sre-eks"
+  cluster_name = "demo-eks-cluster"
 }
 
 
@@ -18,7 +18,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name                 = "lekshmi-vpc"
+  name                 = "demo-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
